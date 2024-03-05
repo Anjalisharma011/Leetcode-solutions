f9fe62b23d1511e3e6ae1aa162ae964783a1f753
Anjalisharma011 <122103601+Anjalisharma011@users.noreply.github.com>
2024-03-05 18:53:54 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (72.90%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string answer;
+        for(int i=0;i<max(word1.length(),word2.length());i++){
+            if(i<word1.length()){
+                answer+=word1[i];
+            }
+             if(i<word2.length()){
+                answer+=word2[i];
+            }
+            
+        }
+    return answer;
+    }
+};
